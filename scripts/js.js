97b1ad55c003d1e2990812c36efa8bdefe7aa7df
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 00:41:12 -0700

    gameboard colors adjusted

--- scripts/js.js
+++ scripts/js.js
@@ -8,8 +8,6 @@ const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 
-console.log(canvas.width)
-
 //  create the game board. each square will be 1/8th the width 
 //  and height of the entire board. 
 //  Make the board cover the entire canvas
@@ -51,9 +49,11 @@ console.log(canvas.width)
 // use this function to create a GameSpace class that will construct GameSpace objects. 
 
 class GameSpace {
-    constructor(row, column, color, isOpen) {
-        this.x = canvas.width*row/8
-        this.y = canvas.height*column/8
+    constructor(column, row, color, isOpen) {
+        // the x position is the width of the entire board(canvas.width) x the column number
+        // and divided by 8. 
+        this.x = canvas.width*column/8
+        this.y = canvas.height*row/8
         this.width = canvas.width/8
         this.height = canvas.height/8
         this.openSpace = isOpen
@@ -63,6 +63,8 @@ class GameSpace {
     renderSpace() {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.strokeStyle = 'black'
+        ctx.strokeRect(this.x, this.y, this.width, this.height)
     }
 }
 
@@ -71,21 +73,24 @@ class GameSpace {
 // square1.renderSpace()
 
 // make the entire gameboard using the for loop from line 36
+// we will make each square using the method outlined above,
+// but will be adding each square into an array. 
 const gameSpaceArray = []
 for (let i=0;i<8;i++) {
     for (let j=0;j<8;j++) {
+        //We have designated "lake" spaces that are not passable. Make them blue. 
         if (
             (i===1 && j===3) || (i===1 && j===4) ||
             (i==2 && j===3) || (i===2 && j===4)  ||
             (i==5 && j===3) || (i===5 && j===4)  ||
             (i==6 && j===3) || (i===6 && j===4) ) {
-            let nextSpace = new GameSpace(i,j, 'blue', false)
+            let nextSpace = new GameSpace(i,j, '#69fcff', false)
             gameSpaceArray.push(nextSpace) 
             } 
         else if (
             i%2===0 && j%2===0 || 
             i%2>0 && j%2>0 ) {
-                let nextSpace = new GameSpace(i,j, 'green', true)
+                let nextSpace = new GameSpace(i,j, '#b4f7a1', true)
                 gameSpaceArray.push(nextSpace)  
             } 
             else {
@@ -97,12 +102,12 @@ for (let i=0;i<8;i++) {
     }
 }
 
-
+// render each square in the array 
 gameSpaceArray.forEach(item => {
-    item.renderSpace()
-    console.log(item)
-   
+    item.renderSpace() 
 })
+
+
 
 
 
