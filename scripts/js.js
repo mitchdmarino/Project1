ba414045767bd9e45b6c4ee6704b696e2eed1850
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 00:41:12 -0700

    finalizes turn basis

--- scripts/js.js
+++ scripts/js.js
@@ -8,6 +8,192 @@ const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 
+/*-------------IMPORTANT FUNCTIONS AND BOOLEANS---------------*/
+
+let gameStatus = true
+// https://masteringjs.io/tutorials/fundamentals/compare-arrays
+// function to compare arrays, returns true if two arrays equal
+function arrayEquals(a,b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.every((val,index) => val === b[index])
+}
+
+// function to reset the board with updated positions
+const newBoard = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // re render the game board 
+    gameSpaceArray.forEach(item => {
+        item.renderSpace() 
+    })
+    //re render each team of soldiers(with the updated value from the move )
+    redTeam.forEach(item => {
+        item.renderSoldier()
+    })
+    blueTeam.forEach(item => {
+        item.renderSoldier()
+        // item.showRank()
+    })
+    
+}
+
+// function to shuffle an array 
+// Fisher-Yates Shuffle 
+function shuffle(array) {
+let currentIndex = array.length,  randomIndex;
+
+// While there remain elements to shuffle.
+while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+    array[randomIndex], array[currentIndex]];
+}
+
+return array;
+}
+
+// function to determine the position of an object (space, redsoldier, bluesoldier)
+// gives position as [column,row]
+// when a player clicks, we need to know which token is being clicked
+// designate by row and column of clicking event 
+function findPosition(x,y) {
+let column = 0
+let row = 0
+switch(true) {
+    case (x<62.5): 
+        column = 0
+        break
+    case (x<125): 
+        column = 1
+        break
+    case (x<187.5): 
+        column = 2
+        break
+    case (x<250): 
+        column = 3
+        break
+    case (x<312.5): 
+        column = 4
+        break
+    case (x<375): 
+        column = 5
+        break
+    case (x<437.5): 
+        column = 6
+        break
+    case (x<500): 
+        column = 7
+        break
+}
+switch(true) {
+    case (y<62.5): 
+        row = 0
+        break
+    case (y<125): 
+        row = 1
+        break
+    case (y<187.5): 
+        row = 2
+        break
+    case (y<250): 
+        row = 3
+        break
+    case (y<312.5): 
+        row = 4
+        break
+    case (y<375): 
+        row = 5
+        break
+    case (y<437.5): 
+        row = 6
+        break
+    case (y<500): 
+        row = 7
+        break
+}
+return [column, row]
+// this gives us our x and y coordinates on the grid( 
+// where X is the column# and Y is the row#)
+// can we combine these into a single 'space'? 
+
+// create an array that stores the information for each space? 
+// build that into our gameSpaceArray 
+// -- needs to have a soldier property 
+}
+
+// handles keyboard input 
+// https://stackoverflow.com/questions/8941183/pass-multiple-arguments-along-with-an-event-object-to-an-event-handler
+const movementHandler = function(item) {
+    // console.log(e.key)
+    const movementFunction = function(e) {
+        switch(e.key) {
+            case('w'):
+            case('ArrowUp'):
+                if (item.moveForward()===false) {
+                    playerTurn()
+                }
+                else {
+                    console.log('player turn is over')
+                    if (gameStatus ===true) {
+                        setTimeout(cpuTurn,2000)
+                    } else {
+                        console.log('game over!')
+                    }
+                }
+                break
+            case('s'):
+            case('ArrowDown'):
+                if (item.moveBack()===false) {
+                    playerTurn()
+                }
+                else {
+                    if (gameStatus ===true) {
+                        setTimeout(cpuTurn,2000)
+                    } else {
+                        console.log('game over!')
+                    }
+                }
+                break
+            case('a'):
+            case('ArrowLeft'):
+                if (item.moveLeft()===false){
+                    playerTurn()
+                } else {
+                    if (gameStatus ===true) {
+                        setTimeout(cpuTurn,2000)
+                    } else {
+                        console.log('game over!')
+                    }
+                }
+                break
+            case('d'):
+            case('ArrowRight'):
+                if (item.moveRight()===false) {
+                    playerTurn()
+                } else {
+                    if (gameStatus ===true) {
+                        setTimeout(cpuTurn,2000)
+                    } else {
+                        console.log('game over!')
+                    }
+                }
+                break
+        }
+        e.stopPropagation()
+        newBoard()
+    }
+    
+    return movementFunction
+}
+
+/*---------------GAME FLOW ----------------------------------*/
+
+// set up the board
 // gamespace class 
 class GameSpace {
     constructor(column, row, color, isOpen) {
@@ -35,12 +221,12 @@ class GameSpace {
         ctx.strokeRect(this.x, this.y, this.width, this.height)
     }
 }
-// make the entire gameboard using for loop
-// add each space into an array to access later 
+// make an array of the entire gameboard using for loop
+// added to array for easy access/manipulation
 const gameSpaceArray = []
 for (let i=0;i<8;i++) {
     for (let j=0;j<8;j++) {
-        //We have designated "lake" spaces that are not passable. Make them blue. 
+        //We have designated "lake" spaces that are not passable. Make them lightblue. 
         if (
             (i===3 && j===1) || (i===4 && j===1) ||
             (i==3 && j===2) || (i===4 && j===2)  ||
@@ -64,38 +250,18 @@ for (let i=0;i<8;i++) {
                  
     }
 }
-// render each square in the array to render the gameboard
-gameSpaceArray.forEach(item => {
-    item.renderSpace() 
-})
+
+
+// create a class for all player pieces on each team
 
 // array to store all possible soldier ranks 
 const soldierRanks = [1,2,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,6,7,7,7,7,7,'F']
-// user Fisher-Yates Shuffle 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-// create a class for all player pieces on each team
 
 // Blue team (player 1)
 class BlueSoldier {
     static total = 24
-    constructor(rank, x, y) {
-        this.rank = rank
+    constructor(x, y) {
+        this.rank = '#'
         this.x = x 
         this.y = y
         // soldier status (alive or not)
@@ -139,7 +305,11 @@ class BlueSoldier {
             gameSpaceArray[currentSpace].currentSoldier = ''
             // now check if opponent is there
             if (gameSpaceArray[potentialSpace].teamColor ==='red') {
-                //battle function 
+                // game checks if there is a battle
+                // if yes, battle and remove inferior piece
+
+                    // then check if the game is won
+                        // end the game 
             }
         }
         else {
@@ -225,8 +395,8 @@ class BlueSoldier {
 // Red Team (CPU)
 class RedSoldier {
     static total = 24
-    constructor(rank, x, y) {
-        this.rank = rank
+    constructor(x, y) {
+        this.rank = '#'
         this.x = x 
         this.y = y
         this.alive=true
@@ -374,170 +544,39 @@ class RedSoldier {
         }
     }
 }
-
-
-// can access each game space to put token in corresponding position 
-// board layout 00 01 02 03 04 05 06 07 
-//              08 09 10 11 12 13 14 15
-//              16 17 18 19 20 21 22 23 ....
-
-
 const redTeam = []
 for (let i=0; i<24; i++) {
     let newX = gameSpaceArray[i].x + 10
     let newY = gameSpaceArray[i].y + 10
-    const soldier = new RedSoldier(3,newX,newY)
+    const soldier = new RedSoldier(newX,newY)
     redTeam.push(soldier)
 }
+// randomize configuration of pieces 
 redTeam.forEach(item => {
-    const pos = item.position
     item.rank = shuffle(soldierRanks).pop()
-    item.renderSoldier()
-    
 })
 
 const blueTeam = []
 for (let i=63; i>39; i--) {
     let newX = gameSpaceArray[i].x + 10
     let newY = gameSpaceArray[i].y + 10
-    const soldier = new BlueSoldier(1,newX,newY)
+    const soldier = new BlueSoldier(newX,newY)
     blueTeam.push(soldier)
    
 }
-blueTeam.forEach(item => {
-    item.renderSoldier()
-    // item.showRank()
-})
 
-  
-const newBoard = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // re render the game board 
-    gameSpaceArray.forEach(item => {
-        item.renderSpace() 
-    })
-    //re render each team of soldiers(with the updated value from the move )
-    redTeam.forEach(item => {
-        item.renderSoldier()
-    })
-    blueTeam.forEach(item => {
-        item.renderSoldier()
-        // item.showRank()
-    })
-    
-}
+// render the initial board
+newBoard()
 
-
-// when a player clicks, we need to know which token is being clicked
-// designate by row and column of clicking event 
-function findPosition(x,y) {
-    let column = 0
-    let row = 0
-    switch(true) {
-        case (x<62.5): 
-            column = 0
-            break
-        case (x<125): 
-            column = 1
-            break
-        case (x<187.5): 
-            column = 2
-            break
-        case (x<250): 
-            column = 3
-            break
-        case (x<312.5): 
-            column = 4
-            break
-        case (x<375): 
-            column = 5
-            break
-        case (x<437.5): 
-            column = 6
-            break
-        case (x<500): 
-            column = 7
-            break
-    }
-    switch(true) {
-        case (y<62.5): 
-            row = 0
-            break
-        case (y<125): 
-            row = 1
-            break
-        case (y<187.5): 
-            row = 2
-            break
-        case (y<250): 
-            row = 3
-            break
-        case (y<312.5): 
-            row = 4
-            break
-        case (y<375): 
-            row = 5
-            break
-        case (y<437.5): 
-            row = 6
-            break
-        case (y<500): 
-            row = 7
-            break
-    }
-    return [column, row]
-    // this gives us our x and y coordinates on the grid( 
-    // where X is the column# and Y is the row#)
-    // can we combine these into a single 'space'? 
-
-    // create an array that stores the information for each space? 
-    // build that into our gameSpaceArray 
-    // -- needs to have a soldier property 
-}
-// handles keyboard input 
-
-// https://stackoverflow.com/questions/8941183/pass-multiple-arguments-along-with-an-event-object-to-an-event-handler
-const movementHandler = function(item) {
-    // console.log(e.key)
-    const movementFunction = function(e) {
-        switch(e.key) {
-            case('w'):
-            case('ArrowUp'):
-                item.moveForward()
-                break
-            case('s'):
-            case('ArrowDown'):
-                item.moveBack()
-                break
-            case('a'):
-            case('ArrowLeft'):
-                item.moveLeft()
-                break
-            case('d'):
-            case('ArrowRight'):
-                item.moveRight()
-                break
-        }
-        e.stopPropagation()
-        newBoard()
-    }
-    return movementFunction
-}
-
-// https://masteringjs.io/tutorials/fundamentals/compare-arrays
-function arrayEquals(a,b) {
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.every((val,index) => val === b[index])
-}
- // player turn. Player clicks on an soldier, and can direct that soldier left, up, right, down using wasd
- // can only click one soldier 
- // can only move one space 
 function playerTurn() {
+    // add click event listener
     canvas.addEventListener('click', e => {
         console.log(`${e.offsetX} is X, ${e.offsetY} is Y`)
+        // use findPosition to get column and row 
         const clickPosition = findPosition(e.offsetX,e.offsetY)
         let clickedSoldier = ""
+        // if column and row matches that of a player soldier (blue team), 
+        // we select that soldier as "clickedSoldier"
         blueTeam.forEach(item => {
             // check if there is a blue soldier on that position
             if (arrayEquals(item.position, clickPosition)) {
@@ -545,17 +584,33 @@ function playerTurn() {
             }
         })
         console.log(clickedSoldier)
-        
-        if (clickedSoldier) {
+        // if there is a clicked soldier, add event listener
+        if (clickedSoldier!=="") {
             document.addEventListener('keydown',movementHandler(clickedSoldier), {once: true})
+        }
+        // if there isn't a clicked soldier, we need to run the function again
+        else {
+            playerTurn()
         }
     },{once: true})
 }
+// player moves the piece
+// ** must be valid space, or has to try again (call the function again ->
+// if the move is invalid )**    
+
+
+// wait 2 seconds, then CPU turn, always will be valid
 function cpuTurn() {
     let cpuTurn = true 
+    // CPU selects random piece, random movement direction, checks if 
+    // it is a viable move. If it is, it moves, if not, it selects 2 new random values and 
+    // checks again until a viable move is found 
+
     while (cpuTurn === true) {
         const randomSoldier = redTeam[Math.floor(Math.random()*redTeam.length)];
+        // the flag cannot move 
         if (randomSoldier.rank === 'F') {
+            // restart the while loop 
             continue
         }
         const randomDirection = Math.floor(Math.random()*4)
@@ -597,17 +652,24 @@ function cpuTurn() {
             }
         }
     }
+    // rerender the board and pieces to see the new move 
     newBoard()
+    if (gameStatus===true) {
+        console.log('next Turn!')
+        playerTurn()
+
+    }
+    else {
+        console.log('GAME OVER')
+    }
 }
-function gameLoop() {
-    playerTurn()
-    setInterval(cpuTurn, 20)
-    
-}
+    // game checks if battle 
+        // if so, battle and result
+            // check if game is won
+                // end the game
 
-
-gameLoop()
-
+        // move on to player turn 
 
 
 
+playerTurn()
