c9889cc9c817771a58ac40f1f380211c871edd3b
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 00:41:12 -0700

    creates checkerboard using OOP

--- scripts/js.js
+++ scripts/js.js
@@ -30,11 +30,47 @@ canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 //     ctx.fillRect(canvas.width*i/8, 0,canvas.width/8, canvas.height/8)
 // }
 
-// apply same logic to create 8 more rows 
+// apply same logic to create 8 more rows
+// add more conditions to create checkerboard appearance 
 
+// for (let i=0;i<8;i++) {
+//     for (let j=0;j<8;j++) {
+//         { if (
+//             i%2===0 && j%2===0 || 
+//             i%2>0 && j%2>0 ) {
+//                 ctx.fillStyle = 'green'
+//             }
+//             else {
+//                 ctx.fillStyle = 'lightgreen'
+//             }
+//             ctx.fillRect(canvas.width*i/8, canvas.width*j/8,canvas.width/8, canvas.height/8)}
+//     }
+// }
+// use this function to create a GameSpace class that will construct GameSpace objects. 
+
+class GameSpace {
+    constructor(row, column) {
+        this.x = canvas.width*row/8
+        this.y = canvas.height*column/8
+        this.width = canvas.width/8
+        this.height = canvas.height/8
+        this.openSpace = true
+        // if openSpace = false, player won't be able to move there
+    }
+    renderSpace() {
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+// make one square with this class 
+// const square1 = new GameSpace(0,0)
+// square1.renderSpace()
+
+// make the entire gameboard using the for loop from line 36
+const gameSpaceArray = []
 for (let i=0;i<8;i++) {
     for (let j=0;j<8;j++) {
-        { if (
+        if (
             i%2===0 && j%2===0 || 
             i%2>0 && j%2>0 ) {
                 ctx.fillStyle = 'green'
@@ -42,9 +78,10 @@ for (let i=0;i<8;i++) {
             else {
                 ctx.fillStyle = 'lightgreen'
             }
-            ctx.fillRect(canvas.width*i/8, canvas.width*j/8,canvas.width/8, canvas.height/8)}
+        gameSpaceArray[i] = new GameSpace(i,j)
+        gameSpaceArray[i].renderSpace()
     }
 }
-// this works, but we want a checkerboard appearrance 
-// when J is odd, switch the if else statement
+
+
 
